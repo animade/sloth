75fed7c2ef30b5d5a430f641c5aeb1dfa63be9b9
Max Stoiber <contact@mstoiber.com>
2015-03-19 14:12:01 +0000

    Revert "Switched from modernizr to ua-parser.js"
    This reverts commit f37ad106f7f58e66a8a8cf4eb68b57bc2da1da6a.
    
    Tried ua-parser.js, didn't work.

--- source/javascripts/initial.js
+++ source/javascripts/initial.js
@@ -1,5 +1,5 @@
+//= require "modernizr/modernizr.js"
 //= require "jquery"
-//= require "ua-parser-js"
 
 // Initialize variables
 var complete = false;
@@ -9,7 +9,6 @@ var counter = 0;
 var loadingwrapper = $("#loadingwrapper");
 queue = new createjs.LoadQueue();
 sceneName = "skyline";
-var parser = new UAParser();
 
 // Put together filenames for easier extensibility
 var src 				= "scenes/" + sceneName;
@@ -73,12 +72,12 @@ queue.on("progress", handleProgress, this);
 queue.on("complete", handleComplete, this);
 queue.on("fileload", handleFileLoad, this);
 
-if (parser.getDevice().type === "mobile") {
-	// Show the mobile warning
-	$("#warning").css("width", "100vw").css("height", "100vh").css("opacity", "1");
-} else {
+if (!Modernizr.touch) {
 	// Start preloading, which initializes the whole thing
 	preload();
+} else {
+	// Otherwise show the mobile warning
+	$("#warning").css("width", "100vw").css("height", "100vh").css("opacity", "1");
 }
 
 // ------------------------------------------
